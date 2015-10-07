c16f9e1c8c8b24c129ac50a1aae6b8a41f6c8dc0
Matteo Bonora <matteo.bonora@marconirovereto.it>
2015-10-07 13:28:48 +0200

    Huge collision system revision Add block collision system

--- BreakTheFuckOut/BreakTheFuckOut/www/game.js
+++ BreakTheFuckOut/BreakTheFuckOut/www/game.js
@@ -1,10 +1,12 @@
+var stage;
 var ball;
-var ballSpdX = 4;
-var ballSpdY = -4;
+var ballSpdX = 5;
+var ballSpdY = -5;
 var pad;
 var AKey = keyboard(65);
 var DKey = keyboard(68);
 var running = true;
+var score = 0;
 
 var movePadDefault = 10;
 var movePadIncreaser = 1.2;  //set the pixel increase every time the pad is moving. giving the pad acceleration while key that move pad is pressed
@@ -18,46 +20,51 @@ var logging = true;
 
 /*  JSON Test  */
 var lev1 = '{"blocks":[' +
-'{"x":"0","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"46","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"92","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"138","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"184","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"230","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"276","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"322","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"368","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"414","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"460","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"506","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"552","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"598","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"644","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"690","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"736","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"782","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"828","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"874","y":"400","width":"45","height":"23","color":"red"},' +
-'{"x":"0","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"46","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"92","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"138","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"184","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"230","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"276","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"322","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"368","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"414","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"460","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"506","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"552","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"598","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"644","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"690","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"736","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"782","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"828","y":"376","width":"45","height":"23","color":"orange"},' +
-'{"x":"874","y":"376","width":"45","height":"23","color":"orange"}]}';
+'{"x":"0","y":"400","width":"45","height":"23","color":"red", "sprite":""},' +
+'{"x":"46","y":"400","width":"45","height":"23","color":"red", "sprite":""},' +
+'{"x":"92","y":"400","width":"45","height":"23","color":"red", "sprite":""},' +
+'{"x":"138","y":"400","width":"45","height":"23","color":"red", "sprite":""}]}';
+//var lev1 = '{"blocks":[' +
+//'{"x":"0","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"46","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"92","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"138","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"184","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"230","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"276","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"322","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"368","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"414","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"460","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"506","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"552","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"598","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"644","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"690","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"736","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"782","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"828","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"874","y":"400","width":"45","height":"23","color":"red"},' +
+//'{"x":"0","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"46","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"92","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"138","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"184","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"230","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"276","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"322","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"368","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"414","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"460","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"506","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"552","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"598","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"644","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"690","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"736","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"782","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"828","y":"376","width":"45","height":"23","color":"orange"},' +
+//'{"x":"874","y":"376","width":"45","height":"23","color":"orange"}]}';
 
 var bricks;
 
@@ -71,71 +78,75 @@ function init() {
       { view: document.getElementById("game-canvas") }
       , true  //antialiasing set to true
     );
-    bricks=JSON.parse(lev1);
-
-    var i,l;
-    for (i = 0, l = bricks.blocks.length; i < l; i++)
-    {
+    bricks = JSON.parse(lev1);
+    
+    var i, l;
+    for (i = 0, l = bricks.blocks.length; i < l; i++) {
+        var b = bricks.blocks[i];
+        b.x = parseInt(b.x);
+        b.y = parseInt(b.y);
+        b.width = parseInt(b.width);
+        b.height = parseInt(b.height);
         var wbTexture = PIXI.Texture.fromImage("../images/" + bricks.blocks[i].color + ".png");
         wb = new PIXI.Sprite(wbTexture);
         wb.position.x = bricks.blocks[i].x;
         wb.position.y = bricks.blocks[i].y;
         stage.addChild(wb);
+        b.sprite = wb;
     }
 
 
-/*var deltaX = 46;
-var deltaY = 24;
-for (var y = 0; y < 7; y++) {
-    for (var x = 0; x < 20; x++) {
-        var color;
-        switch (y) {
-            case 0:
-                color = "../images/violet.png";
-                break;
-            case 1:
-                color = "../images/indigo.png";
-                break;
-            case 2:
-                color = "../images/blue.png";
-                break;
-            case 3:
-                color = "../images/green.png";
-                break;
-            case 4:
-                color = "../images/yellow.png";
-                break;
-            case 5:
-                color = "../images/orange.png";
-                break;
-            case 6:
-                color = "../images/red.png";
-                break;
+    /*var deltaX = 46;
+    var deltaY = 24;
+    for (var y = 0; y < 7; y++) {
+        for (var x = 0; x < 20; x++) {
+            var color;
+            switch (y) {
+                case 0:
+                    color = "../images/violet.png";
+                    break;
+                case 1:
+                    color = "../images/indigo.png";
+                    break;
+                case 2:
+                    color = "../images/blue.png";
+                    break;
+                case 3:
+                    color = "../images/green.png";
+                    break;
+                case 4:
+                    color = "../images/yellow.png";
+                    break;
+                case 5:
+                    color = "../images/orange.png";
+                    break;
+                case 6:
+                    color = "../images/red.png";
+                    break;
+            }
+            var wbTexture = PIXI.Texture.fromImage(color);
+            wb = new PIXI.Sprite(wbTexture);
+            wb.position.x = x * deltaX;
+            console.log(y * deltaY);
+            wb.position.y = y * deltaY + 100;
+            stage.addChild(wb);
         }
-        var wbTexture = PIXI.Texture.fromImage(color);
-        wb = new PIXI.Sprite(wbTexture);
-        wb.position.x = x * deltaX;
-        console.log(y * deltaY);
-        wb.position.y = y * deltaY + 100;
-        stage.addChild(wb);
-    }
-}*/
+    }*/
 
 
-//create pad
-var padTexture = PIXI.Texture.fromImage("../images/pad.png");
-pad = new PIXI.Sprite(padTexture);
-pad.position.x = width / 2 - 11.5;
-pad.position.y = height - 23;
-stage.addChild(pad);
+    //create pad
+    var padTexture = PIXI.Texture.fromImage("../images/pad.png");
+    pad = new PIXI.Sprite(padTexture);
+    pad.position.x = width / 2 - 11.5;
+    pad.position.y = height - 23;
+    stage.addChild(pad);
+    var ballTexture = PIXI.Texture.fromImage("../images/ball.png");
+    ball = new PIXI.Sprite(ballTexture);
+    ball.position.x = width / 2 - 11.5;
+    ball.position.y = height - 46;
+    stage.addChild(ball);
 
-var ballTexture = PIXI.Texture.fromImage("../images/ball.png");
-ball = new PIXI.Sprite(ballTexture);
-ball.position.x = width / 2 - 11.5;
-ball.position.y = height - 46;
-stage.addChild(ball);
-
-requestAnimationFrame(update);
+    requestAnimationFrame(update);
 }
 
 AKey.press = function () {
@@ -199,6 +210,9 @@ function keyboard(keyCode) {
 
 function update() {
 
+    ball.position.x += ballSpdX;
+    ball.position.y += ballSpdY;
+
     CheckCollisions();
 
     if (aPressed) {
@@ -223,45 +237,47 @@ function update() {
 }
 
 function CheckCollisions() {
+    var collisionX = false, collisionY = false;
 
-    if (ballSpdY > 0 && ball.position.y + ball.height >= pad.position.y && (ball.position.x + ball.width > pad.position.x && ball.position.x < pad.position.x + pad.width))
-    {
-        ballSpdY = -ballSpdY;
+    if (ballSpdY > 0 && ball.position.y + ball.height >= pad.position.y && (ball.position.x + ball.width > pad.position.x && ball.position.x < pad.position.x + pad.width)) {
+        collisionY = true;
         if (logging)
             console.log("Collision with pad");
     }
-
-    if ((ball.position.x + ballSpdX) > 0 && (ball.position.x + ballSpdX) < (width - ball.width)) {
-        ball.position.x += ballSpdX;
+    for (var i = 0; i < bricks.blocks.length; i++) {
+        var b = bricks.blocks[i];
+        if ((ball.position.y <= b.y + b.height && ball.position.y + ball.height >= b.y) && (ball.position.x <= b.x + b.width && ball.position.x + ball.width >= b.x)) {
+            stage.removeChild(b.sprite);
+            bricks.blocks.splice(i, 1);
+            score += 100;
+            collisionY = true;
+            if (logging)
+                console.log("Collision with block " + i);
+        }
     }
-    else {
-        ballSpdX = -ballSpdX;
+
+    if (ball.position.x + ball.width >= width || ball.position.x <= 0) {
+        collisionX = true;
         if (logging)
             console.log("Collision with bounds");
     }
-    if (ball.position.y + ballSpdY > 0)
-        ball.position.y += ballSpdY;
-
-    else {
-        ballSpdY = -ballSpdY;
+    if (ball.position.y <= 0) {
+        collisionY = true;
         if (logging)
-            console.log("Collision with bounds");
+            console.log("Collision with top bound");
     }
 
-    if (ball.position.y + ballSpdY >= height - ball.height) {
+    if (ball.position.y >= height - ball.height) {
         ball.position.y = height - ball.height;
         running = false;
         if (logging)
             console.log("Game Over");
     }
-
-    var i, b, l;
-    for (i = 0, l = bricks.blocks.length; i < l; i++) {
-        b = bricks.blocks[i];
-        if ((ball.position.y <= b.y + b.height && ball.position.y + ball.height >= b.y) && (ball.position.x <= b.x + b.width && ball.position.x + ball.width >= b.x));
-        //if (logging)
-            //console.log("Collision with block " + i);
-    }
+    
+    if (collisionX)
+        ballSpdX = -ballSpdX;
+    if (collisionY)
+        ballSpdY = -ballSpdY;
 
     function refr() {
         renderer.render(stage);
