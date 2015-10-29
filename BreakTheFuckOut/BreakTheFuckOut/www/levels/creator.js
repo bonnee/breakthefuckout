9a5a5c238eb4f5086d30c3ce127338cf0608a5e3
Bonny <bonora.matteo@gmail.com>
2015-10-29 21:26:55 +0100

    Align level 1 to grid

--- BreakTheFuckOut/BreakTheFuckOut/www/levels/creator.js
+++ BreakTheFuckOut/BreakTheFuckOut/www/levels/creator.js
@@ -35,18 +35,50 @@ PIXI.interaction.InteractionManager.prototype.onMouseMove = function (event) {
     var partY = event.clientY - rect.top / 2;
     selectedBrick.position.x = Math.floor(partX / 46) * 46;
     selectedBrick.position.y = Math.floor(partY / 24) * 24;
-
-    //console.log(selectedBrick.position.x + ", " + selectedBrick.position.y);
     requestAnimationFrame(Update);
 }
 
 PIXI.interaction.InteractionManager.prototype.onMouseDown = function (event) {
-    //bricks.blocks.push({ x: selectedBrick.position.x, y: selectedBrick.position.y, sprite: selectedBrick.sprite, color: selectedBrick.color });
-    var tx = selectedBrick;
-    stage.addChild(tx);
-    console.log("Cleeck");
+    bricks.blocks.push({ x: selectedBrick.position.x, y: selectedBrick.position.y, width: selectedBrick.width, height: selectedBrick.height, sprite: selectedBrick, color: selectedBrick.color });
+    var s = new PIXI.Sprite(PIXI.Texture.fromImage("../../images/bricks/" + selectedBrick.color));
+    s.position.x = selectedBrick.position.x;
+    s.position.y = selectedBrick.position.y;
+    s.width = selectedBrick.width;
+    s.height = selectedBrick.height;
+    stage.addChild(s);
+    requestAnimationFrame(Update);
 }
 
+function LoadObjects() {
+    bricks = JSONLoader(levName + ".json");
+    var i, l;
+    for (i = 0, l = bricks.blocks.length; i < l; i++) {
+        var b = bricks.blocks[i];
+        b.x = parseInt(b.x);
+        b.y = parseInt(b.y);
+        b.width = parseInt(b.width);
+        b.height = parseInt(b.height);
+        b.score = parseInt(b.score);
+        var wbTexture = PIXI.Texture.fromImage("../../images/bricks/" + b.color);
+        wb = new PIXI.Sprite(wbTexture);
+        wb.position.x = b.x;
+        wb.position.y = b.y;
+        stage.addChild(wb);
+        b.sprite = wb;
+    }
+    var graphics = new PIXI.Graphics().lineStyle(1, 0x808080);
+    for(i = 1;i < width / 46; i++) {
+        graphics.moveTo((i * 46)-1, 0);
+        graphics.lineTo((i * 46)-1, height);
+    }
+    for(i = 1;i < height / 24; i++) {
+        graphics.moveTo(0, (i * 24)-1);
+        graphics.lineTo(width, (i * 24)-1);
+    }
+    stage.addChild(graphics);
+
+    requestAnimationFrame(Update);
+}
 
 //      Load a JSON file from specified URL
 function JSONLoader(url) {
@@ -68,37 +100,6 @@ function JSONLoader(url) {
 
     request.send();
     return data;
-}
-
-function LoadObjects() {
-    bricks = JSONLoader(levName + ".json");
-    var i, l;
-    for (i = 0, l = bricks.blocks.length; i < l; i++) {
-        var b = bricks.blocks[i];
-        b.x = parseInt(b.x);
-        b.y = parseInt(b.y);
-        b.width = parseInt(b.width);
-        b.height = parseInt(b.height);
-        b.score = parseInt(b.score);
-        var wbTexture = PIXI.Texture.fromImage("../../images/bricks/" + b.color + ".png");
-        wb = new PIXI.Sprite(wbTexture);
-        wb.position.x = b.x;
-        wb.position.y = b.y;
-        stage.addChild(wb);
-        b.sprite = wb;
-    }
-    var graphics = new PIXI.Graphics().lineStyle(1, 0x808080);
-    for(i = 0;i < width / 46; i++) {
-        graphics.moveTo((i * 46)-1, 0);
-        graphics.lineTo((i * 46)-1, height);
-    }
-    for(i = 0;i < height / 24; i++) {
-        graphics.moveTo(0, (i * 24)-1);
-        graphics.lineTo(width, (i * 24)-1);
-    }
-    stage.addChild(graphics);
-
-    requestAnimationFrame(Update);
 }
 
 function LoadBricks() {
@@ -125,6 +126,7 @@ function SelectBrick(name) {
 
     stage.removeChild(selectedBrick);
     selectedBrick=new PIXI.Sprite(PIXI.Texture.fromImage("../../images/bricks/" + name));
+    selectedBrick.color = name;
     stage.addChild(selectedBrick);
     requestAnimationFrame(Update);
 }
